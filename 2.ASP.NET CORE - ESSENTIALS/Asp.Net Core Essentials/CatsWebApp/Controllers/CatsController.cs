39e40a4fb81e1ecbbce5236015af7e3df0e37e2e
Daniel Kondov <danielkondov91@gmail.com>
2017-11-04 19:12:46 +0200

    Lab

---
+++ 2.ASP.NET CORE - ESSENTIALS/Asp.Net Core Essentials/CatsWebApp/Controllers/CatsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatsWebApp.Controllers
+{
+    public class CatsController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(Create), new { name = "Vankata" });
+        }
+
+    }
+}
