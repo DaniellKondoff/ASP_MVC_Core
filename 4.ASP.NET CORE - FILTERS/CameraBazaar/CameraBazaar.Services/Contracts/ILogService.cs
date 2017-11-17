7706e2b3f801e08942271ed17f1e307f99a73e23
Daniel Kondov <dkondov@provident.com>
2017-11-17 18:09:04 +0200

    Log Filter

---
+++ 4.ASP.NET CORE - FILTERS/CameraBazaar/CameraBazaar.Services/Contracts/ILogService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CameraBazaar.Services.Contracts
+{
+    public interface ILogService
+    {
+        void Create(string logData);
+    }
+}
---
+++ 4.ASP.NET CORE - FILTERS/CameraBazaar/CameraBazaar.Web/Infrastructure/Filters/LogAttribute.cs
@@ -0,0 +1,37 @@
+﻿using CameraBazaar.Services.Contracts;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CameraBazaar.Web.Infrastructure.Filters
+{
+    public class LogAttribute : ActionFilterAttribute
+    {
+        private string controller;
+        private string action;
+
+        public LogAttribute(string controller, string action)
+        {
+            this.controller = controller;
+            this.action = action;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            var dateTime = DateTime.UtcNow.ToLongDateString();
+            var ipAddress = context.HttpContext.Connection.LocalIpAddress;
+            var user = context.HttpContext.User.Identity.IsAuthenticated ? context.HttpContext.User.Identity.Name : "Anonymos";
+
+            var logService = context.HttpContext.RequestServices.GetService<ILogService>();
+            var logData = $"{dateTime} - {ipAddress} - {user} - {controller}.{action}";
+
+            if (context.ExceptionHandled)
+            {
+
+            }
+            logService.Create(logData);
+        }
+
+        
+    }
+}
