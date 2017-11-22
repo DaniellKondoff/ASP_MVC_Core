e93754e87c996cf83b72fc1a38a6a5fdb3a72488
Daniel Kondov <danielkondov91@gmail.com>
2017-11-23 00:31:30 +0200

    StartUp Project

---
+++ 5.ASP.NET CORE - IDENTITY/LearningSystem/LearningSystem/LearningSystem.Web/Infrastructure/Mapping/IHaveCustomMapping.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace LearningSystem.Web.Infrastructure.Mapping
+{
+    public interface IHaveCustomMapping
+    {
+        void ConfigureMapping(Profile profile);
+    }
+}
---
+++ 5.ASP.NET CORE - IDENTITY/LearningSystem/LearningSystem/LearningSystem.Web/Startup.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using LearningSystem.Data;
+using LearningSystem.Data.Models;
+using LearningSystem.Web.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LearningSystem.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<LearningSystemDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<LearningSystemDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAutoMapper();
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            app.UseDatabaseMigration();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
